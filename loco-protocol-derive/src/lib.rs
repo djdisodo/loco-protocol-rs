9ffc0e5a9cfb54b709e0e0bb4f682deaa1766450
djdisodo <djdisodo@gmail.com>
2020-12-05 14:11:45 +0900

    fix

--- loco-protocol-derive/src/lib.rs
+++ loco-protocol-derive/src/lib.rs
@@ -4,22 +4,22 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{self, Ident, DeriveInput};
 
-#[proc_macro_derive(NameCommandPair)]
+#[proc_macro_derive(NamedCommandPair)]
 pub fn name_command_pair(input: TokenStream) -> TokenStream {
     let derive_input: DeriveInput = syn::parse(input).unwrap();
     let name = derive_input.ident;
     let name_uppercase = Ident::new(&name.to_string().to_uppercase(), name.span());
 
     let token_stream = quote! {
-        impl crate::command::NamedCommand for #name {
+        impl loco_protocol::command::NamedCommand for #name {
             const NAME: &'static str = &stringify!(#name_uppercase);
         }
 
-        impl<T: crate::command::PairedCommandRequest<#name>> crate::command::NamedCommand for T {
+        impl<T: loco_protocol::command::PairedCommandRequest<#name>> loco_protocol::command::NamedCommand for T {
             const NAME: &'static str = &stringify!(#name_uppercase);
         }
 
-        impl<T: crate::command::PairedCommandResponse<#name>> crate::command::NamedCommand for T {
+        impl<T: loco_protocol::command::PairedCommandResponse<#name>> loco_protocol::command::NamedCommand for T {
             const NAME: &'static str = &stringify!(#name_uppercase);
         }
     };
@@ -27,14 +27,14 @@ pub fn name_command_pair(input: TokenStream) -> TokenStream {
     token_stream.into()
 }
 
-#[proc_macro_derive(NameCommand)]
+#[proc_macro_derive(NamedCommand)]
 pub fn name_command(input: TokenStream) -> TokenStream {
     let derive_input: DeriveInput = syn::parse(input).unwrap();
     let name = derive_input.ident;
     let name_uppercase = Ident::new(&name.to_string().to_uppercase(), name.span());
 
     let token_stream = quote! {
-        impl crate::command::NamedCommand for #name {
+        impl loco_protocol::command::NamedCommand for #name {
             const NAME: &'static str = &stringify!(#name_uppercase);
         }
     };
@@ -48,7 +48,7 @@ pub fn request_command(input: TokenStream) -> TokenStream {
     let name = derive_input.ident;
 
     let token_stream = quote! {
-        impl crate::command::PairedCommandRequest for #name {}
+        impl loco_protocol::command::PairedCommandRequest for #name {}
     };
 
     token_stream.into()
@@ -60,7 +60,7 @@ pub fn response_command(input: TokenStream) -> TokenStream {
     let name = derive_input.ident;
 
     let token_stream = quote! {
-        impl crate::command::PairedCommandResponse for #name {}
+        impl loco_protocol::command::PairedCommandResponse for #name {}
     };
 
     token_stream.into()
